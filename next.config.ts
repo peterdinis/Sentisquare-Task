a74e3dd7cfc4b175f7536170a00fa5f71cbe64c6
peterdinis <petter1@pobox.sk>
2025-09-30 20:49:34 +0200

    feat: Start to adding components and helper files WIP

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    typedEnv: true
+  }
 };
 
 export default nextConfig;
